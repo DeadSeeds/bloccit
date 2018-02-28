d9ddba1ef6a41958b327ba95f44b5d2ecaafc6cf
Joshua Brown <xxdeadseeds@gmail.com>
2018-02-27 18:25:31 -0800

    assignment 27 complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ActiveRecord::Base
   before_save { self.email = email.downcase if email.present? }
+  before_save :capitalize_name
 
-  validates :name, length: { minimum:1, maximum: 100 }, presence: true
+  validates :name, length: { minimum: 1, maximum: 100 }, presence: true
   validates :password, presence: true, length: { minimum: 6 }, if: "password_digest.nil?"
   validates :password, length: { minimum: 6 }, allow_blank: true
   validates :email,
@@ -9,5 +10,10 @@ class User < ActiveRecord::Base
             uniqueness: { case_sensitive: false },
             length: { minimum: 3, maximum: 254 }
 
-  has_secure_password        
+  def capitalize_name
+    self.name = name.split.map(&:capitalize).join(' ')
+  end
+
+
+  has_secure_password
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,19 +3,41 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   let(:user) { User.create!(name: "Bloccit User", email: "user@bloccit.com", password: "password") }
 
-  it { is_expected.to validate_presence_of(:name) }
-  it { is_expected.to validate_length_of(:name).is_at_least(1) }
+  describe "Validations" do
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_length_of(:name).is_at_least(1) }
 
-  # Shoulda tests for email
-  it { is_expected.to validate_presence_of(:email) }
-  it { is_expected.to validate_uniqueness_of(:email) }
-  it { is_expected.to validate_length_of(:email).is_at_least(3) }
-  it { is_expected.to allow_value("user@bloccit.com").for(:email) }
+    # Shoulda tests for email
+    context "email" do
+      subject { User.new(name: "name") }
 
-  # Shoulda tests for password
-  it { is_expected.to validate_presence_of(:password) }
-  it { is_expected.to have_secure_password }
-  it { is_expected.to validate_length_of(:password).is_at_least(6) }
+      it { is_expected.to validate_presence_of(:email) }
+      it { is_expected.to validate_uniqueness_of(:email) }
+      it { is_expected.to validate_length_of(:email).is_at_least(3) }
+      it { is_expected.to allow_value("user@bloccit.com").for(:email) }
+    end
+
+    # Shoulda tests for password
+    it { is_expected.to validate_presence_of(:password) }
+    it { is_expected.to have_secure_password }
+    it { is_expected.to validate_length_of(:password).is_at_least(6) }
+  end
+
+  describe "Callbacks" do
+    describe "before save" do
+      it "capitalizes name" do
+        user = User.new(
+          name: "NoT CapiTalIZED",
+          email: "test@test.test",
+          password: "password"
+        )
+
+        user.save!
+
+        expect(user.name).to eq "Not Capitalized"
+      end
+    end
+  end
 
   describe "attributes" do
     it "should have name and email attributes" do
@@ -35,5 +57,7 @@ RSpec.describe User, type: :model do
       expect(user_with_invalid_email).to_not be_valid
     end
   end
+
+  describe ""
 
 end
