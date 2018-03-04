a33fd716e2a43129a8a8d75dd54221aaead141ae
Joshua Brown <xxdeadseeds@gmail.com>
2018-03-04 13:30:29 -0800

    checkpoint 28 complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   before_save { self.email = email.downcase if email.present? }
 
-  validates :name, length: { minimum:1, maximum: 100 }, presence: true
+  validates :name, length: { minimum: 1, maximum: 100 }, presence: true
   validates :password, presence: true, length: { minimum: 6 }, if: "password_digest.nil?"
   validates :password, length: { minimum: 6 }, allow_blank: true
   validates :email,
@@ -9,5 +9,5 @@ class User < ActiveRecord::Base
             uniqueness: { case_sensitive: false },
             length: { minimum: 3, maximum: 254 }
 
-  has_secure_password        
+  has_secure_password
 end
