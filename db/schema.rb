93f3a52dece03d8fe123cb6db883fad757b04198
Joshua Brown <xxdeadseeds@gmail.com>
2018-03-12 01:47:06 -0700

    checkpoint 32 complete

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180307024036) do
+ActiveRecord::Schema.define(version: 20180312054344) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string   "title"
@@ -78,6 +78,7 @@ ActiveRecord::Schema.define(version: 20180307024036) do
     t.string   "string"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.integer  "role"
   end
 
 end
