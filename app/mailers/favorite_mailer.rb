938f10d30c47ce2208c3fb6b803dea38fad096d2
Joshua Brown <xxdeadseeds@gmail.com>
2018-03-21 14:54:19 -0700

    checkpoint 35 complete

--- app/mailers/favorite_mailer.rb
+++ app/mailers/favorite_mailer.rb
@@ -12,4 +12,15 @@ class FavoriteMailer < ApplicationMailer
 
     mail(to: user.email, subject: "New comment on #{post.title}")
   end
+
+  def new_post(post)
+    headers["Message-ID"] = "<posts/#{post.id}@salty-dusk-86449.herokuapp.com>"
+    headers["In-Reply-To"] = "<post/#{post.id}@salty-dusk-86449.herokuapp.com>"
+    headers["References"] = "<post/#{post.id}@salty-dusk-86449.herokuapp.com>"
+
+    @post = post
+
+    mail(to: post.user.email, subject: "You've favorited #{post.title} and will receive updates about it")
+  end
+
 end
